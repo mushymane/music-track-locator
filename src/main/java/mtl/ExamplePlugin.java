a2e3946032672fe0e2417cbdb0ebad927d19260d
Luigi Lantin <mushymanee@gmail.com>
2024-08-02 13:04:34 -0700

    setting up

--- src/main/java/mtl/ExamplePlugin.java
+++ src/main/java/mtl/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package mtl;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
--- src/test/java/mtl/mtlTest.java
+++ src/test/java/mtl/mtlTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package mtl;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class mtlTest
 {
 	public static void main(String[] args) throws Exception
 	{
